aabcdd0dafd9e6040d8ca7f192319f7758087030
Stephen Eley <sfeley@gmail.com>
2009-02-28 19:11:26 -0500

    Environment gems setup

--- config/environment.rb
+++ config/environment.rb
@@ -15,6 +15,11 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
+  config.gem "haml", :version => '>=2.0'
+  config.gem "rspec", :version => '>=1.1.12'
+  config.gem "rspec-rails", :version => '>=1.1.12'
+  config.gem "cucumber", :version => ">=0.1"
+  
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
